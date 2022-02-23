2572bc1e52bcddb66eabcbc4ea49b2604bc2ce7d
Takahiro Ido <juve.taka@gmail.com>
2022-02-23 16:45:03 +0900

    commit station1

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
   return (
     <div>
+      <header>Doinkyaアプリ</header>
       <h2>Hello, world!</h2>
     </div>
   )
