05c48312d081bebc92e0717e08dc72ddf34d492e
Takahiro Ido <juve.taka@gmail.com>
2022-02-26 23:28:53 +0900

    commit station4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState } from 'react'
 import './App.css'
 
 /**
  *
  * @type {React.FC}
  */
+
+const INITIAL_URL =
+  'https://images.dog.ceo/breeds/terrier-dandie/n02096437_1641.jpg'
+
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(INITIAL_URL)
+
+  const urlReset = () => setDogUrl(INITIAL_URL)
   return (
     <div>
       <header></header>
@@ -15,7 +23,7 @@ export const App = () => {
       <p>
         犬の画像を表示するサイトです。
         <br />
-        <img src="https://images.dog.ceo/breeds/terrier-dandie/n02096437_1641.jpg"></img>
+        <img src={dogUrl}></img>
       </p>
     </div>
   )
